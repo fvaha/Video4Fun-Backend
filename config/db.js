e000da05e9191fd54fc7efc6bde5cf4236c83849
Vaha <vaha@vaha.net>
2024-12-13 22:42:35 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2");
 
 // Database connection configuration
 const db = mysql.createConnection({
-  host: "89.216.114.190", // IP address of your MySQL server
-  user: "adminvideo4",     // Your MySQL username
-  password: "vfpcjbP5swBL0kP", // Your MySQL password
-  database: "video4",      // The database name you want to use
-  port: 3333,              // Port number for MySQL
+  host: "", // IP address of your MySQL server
+  user: "",     // Your MySQL username
+  password: "", // Your MySQL password
+  database: "",      // The database name you want to use
+  port: ,              // Port number for MySQL
 });
 
 // Establish connection to MySQL
